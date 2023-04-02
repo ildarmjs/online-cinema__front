b1bf2927bc5239250dd8333eddce550ac267268f
Ildar <ildar.1795@gmail.com>
2023-04-02 14:56:26 +0400

    change file SingleMovie

--- app/components/screens/single-movie/SingleMovie.tsx
+++ app/components/screens/single-movie/SingleMovie.tsx
@@ -4,23 +4,25 @@ import SubHeading from '@/components/ui/heading/SubHeading'
 import VideoPlayer from '@/components/ui/video-player/VideoPlayer'
 import Meta from '@/utils/meta/Meta'
 import dynamic from 'next/dynamic'
-import {FC} from 'react'
+import { FC } from 'react'
 import { IMoviePage } from '../../../../pages/movie/[slug]'
 import Content from './Content/Content'
-import { useUpdateCountOpened } from './useUpdateCountOpened'
-
+// import { useUpdateCountOpened } from './useUpdateCountOpened'
 
 const DynamicPlayer = dynamic(() => import('@/ui/video-player/VideoPlayer'), {
-	ssr: false,
+	ssr: false
 })
-const DynamicRateMovie = dynamic(() => import('@/components/screens/single-movie/RateMovie/RateMovie'), {
-	ssr: false,
-})
+const DynamicRateMovie = dynamic(
+	() => import('@/components/screens/single-movie/RateMovie/RateMovie'),
+	{
+		ssr: false
+	}
+)
 
-const SingleMovie: FC<IMoviePage> = ({movie, similarMovies}) => {
+const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 	// useUpdateCountOpened(movie.slug)
 
-  return (
+	return (
 		<Meta title={movie.title} description={`Watch ${movie.title}`}>
 			<Banner
 				imagePath={movie.bigPoster}
